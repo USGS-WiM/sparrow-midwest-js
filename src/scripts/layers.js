93b9c30be04e3d9b8b2039d85340f5110508af8e
esmyers <emyers@usgs.gov>
2019-04-08 16:31:31 -0500

    first commit

---
+++ src/scripts/layers.js
@@ -0,0 +1,218 @@
+/**
+ * Created by bdraper on 4/27/2015.
+ */
+
+/**UPDATE impoartant! -- when updating to a new application, make sure that all visiblelayer Ids match the corresponding REST service layer.
+    Also make sure that titles are appropriate
+ **/
+var allLayers;
+
+require([
+    "esri/geometry/Extent",
+    "esri/layers/WMSLayerInfo",
+    "esri/layers/FeatureLayer",
+    "esri/layers/ImageParameters",
+    "esri/layers/LayerDrawingOptions",
+    "dojo/domReady!"
+], function (Extent, WMSLayerInfo, FeatureLayer, ImageParameters, LayerDrawingOptions) {
+    var sparrowOverlay;
+    if ($("#radio1")[0].checked == true) {
+        sparrowOverlay = 0;
+    } else {
+        sparrowOverlay = 1;
+    }
+
+    var layerOptions = new LayerDrawingOptions();
+    layerOptions.scaleSymbols = true;
+
+    allLayers = [
+        {
+            groupHeading: "Nutrient Model",
+            showGroupHeading: true,
+            includeInLayerList: true,
+            layers: {
+                "SPARROW Model Results": {
+                    url: serviceBaseURL,
+                    visibleLayers: [sparrowOverlay],
+                    options: {
+                        id: "SparrowRanking",
+                        opacity: 0.75,
+                        visible: true,
+                        layerDrawingOptions: layerOptions
+                    },
+                    wimOptions: {
+                        type: "layer",
+                        layerType: "agisDynamic",
+                        includeInLayerList: true,
+                        hasOpacitySlider: true,
+                        hasZoomto: false,
+                        includeLegend: false
+                    }
+                }
+            }
+        },
+        {
+            groupHeading: "Model Calibration Sites",
+            showGroupHeading: true,
+            includeInLayerList: true,
+            layers: {
+                "Phosphorus Calibration Sites": {
+                    url: serviceBaseURL,
+                    visibleLayers: [36],
+                    options: {
+                        id: "phosCalibration",
+                        opacity: 0.85,
+                        visible: false
+                    },
+                    wimOptions: {
+                        type: "layer",
+                        layerType: "agisDynamic",
+                        includeInLayerList: true,
+                        zoomScale: 144448,
+                        hasOpacitySlider: true,
+                        hasZoomto: false,
+                        includeLegend: true
+                    }
+                },
+                "Nitrogen Calibration Sites": {
+                    url: serviceBaseURL,
+                    visibleLayers: [37],
+                    options: {
+                        id: "nitroCalibration",
+                        visible: false,
+                        opacity: 0.85
+                    },
+                    wimOptions: {
+                        type: "layer",
+                        layerType: "agisDynamic",
+                        includeInLayerList: true,
+                        hasOpacitySlider: true,
+                        hasZoomto: false,
+                        includeLegend: false
+                    }
+                },
+                "Streamflow Calibration Sites": {
+                    url: serviceBaseURL,
+                    visibleLayers: [38],
+                    options: {
+                        id: "streamflowCalibration",
+                        visible: false,
+                        opacity: 0.85
+                    },
+                    wimOptions: {
+                        type: "layer",
+                        layerType: "agisDynamic",
+                        includeInLayerList: true,
+                        hasOpacitySlider: true,
+                        hasZoomto: false,
+                        includeLegend: false
+                    }
+                },
+                "Suspended Sediment Calibration Sites": {
+                    url: serviceBaseURL,
+                    visibleLayers: [39],
+                    options: {
+                        id: "sedimentCalibration",
+                        visible: false,
+                        opacity: 0.85
+                    },
+                    wimOptions: {
+                        type: "layer",
+                        layerType: "agisDynamic",
+                        includeInLayerList: true,
+                        hasOpacitySlider: true,
+                        hasZoomto: false,
+                        includeLegend: false
+                    }
+                }
+            }
+        },
+        {
+            groupHeading: "Auxiliary Layers",
+            showGroupHeading: true,
+            includeInLayerList: true,
+            layers: {
+/*                 "Midcontinent Reaches": {
+                    url: serviceBaseURL,
+                    visibleLayers: [20],
+                    options: {
+                        id: "streams",
+                        visible: false
+                    },
+                    wimOptions: {
+                        type: "layer",
+                        layerType: "agisDynamic",
+                        includeInLayerList: true,
+                        hasOpacitySlider: true,
+                        hasZoomto: false,
+                        includeLegend: false
+                    }
+                },
+                "Reaches > 4.2 m³ per second": {
+                    url: serviceBaseURL,
+                    visibleLayers: [22],
+                    options: {
+                        id: "gt4.2",
+                        visible: false
+                    },
+                    wimOptions: {
+                        type: "layer",
+                        layerType: "agisDynamic",
+                        includeInLayerList: true,
+                        hasOpacitySlider: true,
+                        hasZoomto: false,
+                        includeLegend: false
+                    }
+                },
+                "Reaches > 28 m³ per second": {
+                    url: serviceBaseURL,
+                    visibleLayers: [21],
+                    options: {
+                        id: "gt28",
+                        visible: false
+                    },
+                    wimOptions: {
+                        type: "layer",
+                        layerType: "agisDynamic",
+                        includeInLayerList: true,
+                        hasOpacitySlider: true,
+                        hasZoomto: false,
+                        includeLegend: false
+                    }
+                },
+                "Reaches > 142 m³ per second": {
+                    url: serviceBaseURL,
+                    visibleLayers: [23],
+                    options: {
+                        id: "gt142",
+                        visible: false
+                    },
+                    wimOptions: {
+                        type: "layer",
+                        layerType: "agisDynamic",
+                        includeInLayerList: true,
+                        hasOpacitySlider: true,
+                        hasZoomto: false,
+                        includeLegend: false
+                    }
+                }, */
+                "Land Use 2002": {
+                    url: "https://gis.wim.usgs.gov/arcgis/rest/services/SWTrends/lu2002_100515_test/ImageServer",
+                    options: {
+                        id: "lu2002",
+                        opacity: 0.5,
+                        visible: false
+                    },
+                    wimOptions: {
+                        type: "layer",
+                        layerType: "agisImage",
+                        includeInLayerList: true,
+                        hasOpacitySlider: true,
+                        hasZoomto: false,
+                        includeLegend: false
+                    }
+                }
+            }
+        }
+    ];
+});
